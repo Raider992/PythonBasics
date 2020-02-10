3202585369225cf94c931c7e1dfbf48f63a50f11
Raider992 <19stargazer92@gmail.com>
2020-02-10 13:10:42 +0300

    Дз к уроку №5

--- file_strings.py
+++ file_strings.py
@@ -2,5 +2,24 @@
 # Об окончании ввода данных свидетельствует пустая строка.
 
 
+def write_strings(*args):
+    res = ''
+    terminate = False
+    for i in args:
+        try:
+            with open('task_1.txt', 'w') as file:
+                file.write()
+        except ValueError as e:
+            if i == '\n':
+                terminate = not terminate
+    return res, terminate
+
 while True:
-    lines =
+    user_input = input("Введите строки, для выхода нажмите Enter повторно\n").split('\n')
+    res_sum, terminate = write_strings(*user_input)
+    sum +=res_sum
+    print(f"сумма: {sum}")
+
+    if terminate:
+        print('Ввод окончен')
+        break
